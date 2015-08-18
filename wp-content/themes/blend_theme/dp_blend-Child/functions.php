47e846d90da920de5093c9b796521aae8bcbeed3
Jared Williams <jaredwilliamsmail@gmail.com>
2015-08-18 03:55:05 +0000

    reset c9 being a b

--- wp-content/themes/blend_theme/dp_blend-Child/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-function dp_child_theme_stylesheet() {
-  wp_enqueue_style('dp_child_css', get_stylesheet_uri(), false, null);
-}
-add_action('wp_enqueue_scripts', 'dp_child_theme_stylesheet', 110);
-
